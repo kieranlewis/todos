8a277b538ea7fd1046a9b699830aa4a72605a690
Kieran Lewis <kieranlewis1334@gmail.com>
2021-12-04 10:48:15 +1300

    removed dist folder from .gitignore

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+ console.log('You are in the right place');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLDJDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG9kb3MvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiIGNvbnNvbGUubG9nKCdZb3UgYXJlIGluIHRoZSByaWdodCBwbGFjZScpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
