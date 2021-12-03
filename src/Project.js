eed2af34bda47f1f988b40711625db1431cc6c1f
Kieran Lewis <kieranlewis1334@gmail.com>
2021-12-04 10:24:19 +1300

    Added project module to create a new project

---
+++ src/Project.js
@@ -0,0 +1,5 @@
+const Project = (title, desc, dueDate, priority) => {
+    return { title, desc, dueDate, priority };
+}
+
+export default Project
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
- console.log('You are in the right place');
+ import Project from './Project';
+ 
+ //Dummy Project
+const today = Project('Today', 'Initial project', new Date(), 5);
+console.log(today);
