62288fe78985f6e65b8d8ea5ed03d11f29120faa
Kieran Lewis <kieranlewis1334@gmail.com>
2021-12-12 17:40:04 +1300

    Dates will now be formatted to the correct dd-MM-yyyy

--- src/Task.js
+++ src/Task.js
@@ -1,3 +1,5 @@
+import { format } from "date-fns";
+
 const Task = (title, dueDate, priority) => {
     return {
         get title() {
--- src/UI.js
+++ src/UI.js
@@ -55,7 +55,8 @@ const submitTaskForm = () => {
     const title = document.querySelector('#taskName');
     const dueDate = document.querySelector('#dueDate');
     const priority = document.querySelector('input[name="priority"]:checked');
-    const newTask = Task(title.value, dueDate.value, priority.value);
+
+    const newTask = Task(title.value, format(new Date(dueDate.value), 'dd-MM-yyyy'), priority.value);
 
     currentProject.addTask(newTask);
 
